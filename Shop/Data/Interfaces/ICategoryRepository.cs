7f29b643cd96af0100586ea7da03460ea27d1e28
sameeranihathe <sameeranihathe@gmail.com>
2018-04-24 09:06:26 +0530

    mock files created

--- Shop/Data/Interfaces/ICategoryRepository.cs
+++ Shop/Data/Interfaces/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shop.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace Shop.Data.Interfaces
 {
     interface ICategoryRepository
     {
+        IEnumerable<Category> Categories { get; }
     }
 }
