87704513e582270e16620ae776b59cc5ed08150c
sameeranihathe <sameeranihathe@gmail.com>
2018-04-24 08:37:35 +0530

    interfaces created

---
+++ Shop/Data/Interfaces/IDrinkRepository.cs
@@ -0,0 +1,17 @@
+﻿using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Interfaces
+{
+    interface IDrinkRepository
+    {
+        IEnumerable<Drink> Drinks { get; }
+
+        IEnumerable<Drink> PreferredDrinks { get; }
+
+        Drink GetDrinkById(int drinkId);
+    }
+}
